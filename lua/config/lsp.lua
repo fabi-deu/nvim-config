f408c94421a9ed12ae8b448802bf93cc9250e84a
fabi-deu <fabi_deu@icloud.com>
2026-05-09 20:39:03 +0200

    feat(lsp): diagnostics

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,3 +1,19 @@
+vim.diagnostic.config {
+	update_in_insert = false,
+	severity_sort = true,
+	float = { border = "rounded", source = "if_many" },
+	underline = { severity = { min = vim.diagnostic.severity.WARN } },
+
+	virtual_text = true,
+	virtual_lines = false,
+
+	jump = { float = true },
+}
+
+
+
+
+
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("clangd")
