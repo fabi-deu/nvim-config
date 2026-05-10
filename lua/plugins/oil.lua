417d2ff5d5224fe7e3c3694fb357dbab56283443
fabi-deu <fabi_deu@icloud.com>
2026-05-10 22:04:14 +0200

    plugin: oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }
+		-- dependencies = { { "nvim-mini/mini.icons", opts = {} } }
+		lazy = false,
+	}
+}
